bbd722c8739cfd2e46140b66063636fffb0ed2ce
crpeiris <cranil@sjp.ac.lk>
2026-06-21 21:45:06 +0530

    static and media created in the project level

--- phoneproject/urls.py
+++ phoneproject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from store import views as storeviews
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', storeviews.storehome),  # This handles the root URL
     path('store/', include('store.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
